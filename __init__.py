f47dbdd6652b1e8e0754e3d314658dca21c38fc9
Martin Eklund <music@teadrinker.net>
2024-11-04 13:07:32 +0100

    Append Material Names option

--- __init__.py
+++ __init__.py
@@ -64,6 +64,7 @@ class teadrinker_quick_export(bpy.types.Operator):
     override_filename: bpy.props.StringProperty(name = "Override Filename", subtype = 'FILE_NAME',             default = '')
     name_from_selection:bpy.props.BoolProperty (name = "Filename from selection",                              default = False)
     selected_only:     bpy.props.BoolProperty  (name = "Export Selected Only",                                 default = False)
+    obj_mat_names:     bpy.props.BoolProperty  (name = "Append Material Names (obj)",                          default = False)
     obj_merge_by_mat:  bpy.props.BoolProperty  (name = "Merge Objects (obj)",                                  default = False)
     obj_triangulate:   bpy.props.BoolProperty  (name = "Triangulate (obj)",                                    default = True)
     scale:             bpy.props.FloatProperty (name = "Scale (only obj/fbx)",                                 default = 1.0)
@@ -98,6 +99,7 @@ class teadrinker_quick_export(bpy.types.Operator):
                         self.override_filename = cfg['override_filename']
                         self.name_from_selection = cfg['name_from_selection']
                         self.selected_only = cfg['selected_only']
+                        self.obj_mat_names = cfg['obj_mat_names']
                         self.obj_merge_by_mat = cfg['obj_merge_by_mat']
                         self.obj_triangulate = cfg['obj_triangulate']
                         self.scale = cfg['scale']
@@ -110,7 +112,7 @@ class teadrinker_quick_export(bpy.types.Operator):
                     export_dir_relative = True
                 except: pass
 
-                save_cfg(settings_fullpath, { 'out_format' : self.out_format, 'scale' : self.scale, 'override_filename' : self.override_filename, 'name_from_selection' : self.name_from_selection, 'selected_only' : self.selected_only, 'obj_merge_by_mat' : self.obj_merge_by_mat ,'obj_triangulate' : self.obj_triangulate ,'dir' : export_dir, 'relative' : export_dir_relative })
+                save_cfg(settings_fullpath, { 'out_format' : self.out_format, 'scale' : self.scale, 'override_filename' : self.override_filename, 'name_from_selection' : self.name_from_selection, 'selected_only' : self.selected_only, 'obj_mat_names' : self.obj_mat_names, 'obj_merge_by_mat' : self.obj_merge_by_mat ,'obj_triangulate' : self.obj_triangulate ,'dir' : export_dir, 'relative' : export_dir_relative })
         else:
             console_print('teadrinker quick export: Warning, settings will not be saved')
 
@@ -142,7 +144,7 @@ class teadrinker_quick_export(bpy.types.Operator):
 
         if self.out_format == 'obj':
             console_print('teadrinker quick export: Writing obj: ' + export_fullpath)
-            bpy.ops.wm.obj_export(filepath=export_fullpath, global_scale=self.scale, check_existing=False, export_selected_objects=self.selected_only, export_triangulated_mesh=self.obj_triangulate, export_material_groups=False, export_object_groups=not self.obj_merge_by_mat) # export_material_groups is same as old API group_by_material param? 
+            bpy.ops.wm.obj_export(filepath=export_fullpath, global_scale=self.scale, check_existing=False, export_selected_objects=self.selected_only, export_triangulated_mesh=self.obj_triangulate, export_material_groups=(not self.obj_merge_by_mat) and self.obj_mat_names, export_object_groups=(not self.obj_merge_by_mat) and (not self.obj_mat_names)) # export_material_groups is same as old API group_by_material param? 
         elif self.out_format == 'fbx':
             console_print('teadrinker quick export: Writing fbx: ' + export_fullpath)
             bpy.ops.export_scene.fbx(filepath=export_fullpath, global_scale=self.scale, check_existing=False, use_selection=self.selected_only)
